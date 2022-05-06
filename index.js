f2b1959a73b7a25ce0327cf66377e6a3b1e15793
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-06 19:03:24 +0300

    Add nodemon, check if username and password match

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.post('/api/auth/sign-in', (req, res) => {
-    console.log(req);
-    res.json({ jwt: 'access_token_from_api' });
+    console.log(req.body);
+    const { username, password } = req.body;
+
+    if (username === 'user@gmail.com') {
+        if (password === 'qwerty') {
+            return res.json({ jwt: 'access_token_from_api' });
+        } else {
+            return res.json({ error: true, message: "Username and password doesn't match" });
+        }
+    } else {
+        return res.json({ error: true, message: "User doesn't exist" });
+    }
 });
 
 app.listen(port, () => {
