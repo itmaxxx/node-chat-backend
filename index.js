266fa7a838dd2dda6e6800a4bcb52e4df449dc93
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-10 12:15:26 +0300

    Add chats images

--- index.js
+++ index.js
@@ -26,16 +26,20 @@ app.get('/api/chats', (req, res) => {
         {
             id: 'chat_1',
             name: 'Pavel Durov',
-            image: 'image_url',
+            image: 'https://static.dw.com/image/43377200_303.jpg',
             lastMessage: 'Your app is better than Telegram'
         },
         {
             id: 'chat_2',
             name: 'ITStep',
-            image: 'image_url',
+            image: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570',
             lastMessage: 'JavaScript webinar tomorrow at 5 PM'
         }
     ];
+
+    // for (let i = 0; i < 1000; i++) {
+    //     chats.push({ id: `chat_1${i}`, name: `Chat #${i}`, image: null, lastMessage: `Last message from chat ${i}` });
+    // }
 
     return res.json({ type: 'ChatsList', data: chats });
 })
