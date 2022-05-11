d960872481dd6294aa4e0e4d4f8f048983c8de11
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-12 00:42:48 +0300

    Improve dummy data

--- index.js
+++ index.js
@@ -27,13 +27,37 @@ app.get('/api/chats', (req, res) => {
             id: 'chat_1',
             name: 'Pavel Durov',
             image: 'https://static.dw.com/image/43377200_303.jpg',
-            lastMessage: 'Your app is better than Telegram'
+            lastMessage: {
+                id: 'message_id_1',
+                chatId: 'chat_1',
+                text: 'Your app looks cool',
+                author: {
+                    id: 'user_id_1',
+                    fullname: 'Pavel Durov',
+                    username: 'durov',
+                    image: 'https://static.dw.com/image/43377200_303.jpg'
+                },
+                createdAt: Date.now(),
+                updatedAt: Date.now()
+            }
         },
         {
             id: 'chat_2',
             name: 'ITStep',
             image: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570',
-            lastMessage: 'JavaScript webinar tomorrow at 5 PM'
+            lastMessage: {
+                id: 'message_id_1',
+                chatId: 'chat_2',
+                text: 'Webinar today at 5 PM',
+                author: {
+                    id: 'user_id_2',
+                    fullname: 'ITStep',
+                    username: 'itstep',
+                    image: 'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570'
+                },
+                createdAt: Date.now(),
+                updatedAt: Date.now()
+            }
         }
     ];
 
@@ -42,7 +66,31 @@ app.get('/api/chats', (req, res) => {
     // }
 
     return res.json({ type: 'ChatsList', data: chats });
-})
+});
+
+app.get('/api/chats/:chatId/messages', (req, res) => {
+    console.log('/api/chats', req.params);
+
+    const messages = [];
+
+    for (let i = 0; i < 20; i++) {
+        messages.push({
+            id: 'message_id_' + i,
+            chatId: req.params.chatId,
+            text: 'Message ' + i,
+            author: {
+                id: 'user_id_1',
+                fullname: 'Max Dmitriev',
+                username: 'itmax',
+                image: 'https://static.dw.com/image/43377200_303.jpg'
+            },
+            createdAt: Date.now() - i,
+            updatedAt: Date.now() - i
+        });
+    }
+
+    return res.json({ type: 'MessagesList', data: messages });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
