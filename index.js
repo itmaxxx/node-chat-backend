583e37f230f6e61ac6b95d2628611922d9efc9a4
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-18 22:59:13 +0300

    Refactor users into array

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const port = 3000;
 
 app.use(express.json());
 
+const users = [
+  {
+    id: 'user_id_1',
+    fullname: 'Max Dmitriev',
+    username: 'itmax',
+    image: 'https://avatars.githubusercontent.com/u/38819868?v=4',
+  },
+  {
+    id: 'user_id_2',
+    fullname: 'Pavel Durov',
+    username: 'durov',
+    image: 'https://static.dw.com/image/43377200_303.jpg',
+  },
+  {
+    id: 'user_id_3',
+    fullname: 'ITStep',
+    username: 'itstep',
+    image:
+      'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570',
+  },
+];
+
 app.post('/api/auth/sign-in', (req, res) => {
   console.log('/api/auth/sign-in', req.body);
   const { username, password } = req.body;
@@ -12,12 +34,7 @@ app.post('/api/auth/sign-in', (req, res) => {
     if (password === 'qwerty') {
       return res.json({
         token: 'access_token_from_api',
-        data: {
-          id: 'user_id_from_backend',
-          fullname: 'Max Dmitriev',
-          username: 'itmax',
-          image: 'https://avatars.githubusercontent.com/u/38819868?v=4',
-        },
+        data: users[0],
       });
     } else {
       return res.json({
@@ -42,12 +59,7 @@ app.get('/api/chats', (req, res) => {
         id: 'message_id_1',
         chatId: 'chat_1',
         text: 'Your app looks cool',
-        author: {
-          id: 'user_id_1',
-          fullname: 'Pavel Durov',
-          username: 'durov',
-          image: 'https://static.dw.com/image/43377200_303.jpg',
-        },
+        author: users[1],
         isAuthor: false,
         createdAt: Date.now(),
         updatedAt: Date.now(),
@@ -62,23 +74,13 @@ app.get('/api/chats', (req, res) => {
         id: 'message_id_1',
         chatId: 'chat_2',
         text: 'Webinar today at 5 PM',
-        author: {
-          id: 'user_id_2',
-          fullname: 'ITStep',
-          username: 'itstep',
-          image:
-            'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570',
-        },
+        author: users[2],
         isAuthor: false,
         createdAt: Date.now(),
         updatedAt: Date.now(),
       },
     },
   ];
-
-  // for (let i = 0; i < 1000; i++) {
-  //     chats.push({ id: `chat_1${i}`, name: `Chat #${i}`, image: null, lastMessage: `Last message from chat ${i}` });
-  // }
 
   return res.json({ type: 'ChatsList', data: chats });
 });
@@ -93,12 +95,7 @@ app.get('/api/chats/:chatId/messages', (req, res) => {
       id: 'message_id_' + i,
       chatId: req.params.chatId,
       text: 'Message hello' + i,
-      author: {
-        id: 'user_id_1',
-        fullname: 'Max Dmitriev',
-        username: 'itmax',
-        image: 'https://static.dw.com/image/43377200_303.jpg',
-      },
+      author: users[Math.floor((Math.random() * 3) % 3)],
       isAuthor: Math.random() >= 0.7,
       createdAt: Date.now() - i,
       updatedAt: Date.now() - i,
@@ -117,12 +114,7 @@ app.post('/api/chats/:chatId/messages', (req, res) => {
       id: 'message_id_new',
       chatId: req.params.chatId,
       text: req.body.message,
-      author: {
-        id: 'user_id_1',
-        fullname: 'Max Dmitriev',
-        username: 'itmax',
-        image: 'https://static.dw.com/image/43377200_303.jpg',
-      },
+      author: users[0],
       isAuthor: true,
       createdAt: Date.now(),
       updatedAt: Date.now(),
