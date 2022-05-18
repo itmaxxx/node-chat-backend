879669bcbe54e50e3209eabdf6a94a475c7a92eb
itmaxxx <dmitrievmaks77@gmail.com>
2022-05-19 00:09:06 +0300

    Refactor chats to global variable

--- index.js
+++ index.js
@@ -26,6 +26,38 @@ const users = [
   },
 ];
 
+const chats = [
+  {
+    id: 'chat_1',
+    name: 'Pavel Durov',
+    image: 'https://static.dw.com/image/43377200_303.jpg',
+    lastMessage: {
+      id: 'message_id_1',
+      chatId: 'chat_1',
+      text: 'Your app looks cool',
+      author: users[1],
+      isAuthor: false,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+  },
+  {
+    id: 'chat_2',
+    name: 'ITStep',
+    image:
+      'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570',
+    lastMessage: {
+      id: 'message_id_1',
+      chatId: 'chat_2',
+      text: 'Webinar today at 5 PM',
+      author: users[2],
+      isAuthor: false,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    },
+  },
+];
+
 app.post('/api/auth/sign-in', (req, res) => {
   console.log('/api/auth/sign-in', req.body);
   const { username, password } = req.body;
@@ -49,38 +81,6 @@ app.post('/api/auth/sign-in', (req, res) => {
 
 app.get('/api/chats', (req, res) => {
   console.log('/api/chats');
-
-  const chats = [
-    {
-      id: 'chat_1',
-      name: 'Pavel Durov',
-      image: 'https://static.dw.com/image/43377200_303.jpg',
-      lastMessage: {
-        id: 'message_id_1',
-        chatId: 'chat_1',
-        text: 'Your app looks cool',
-        author: users[1],
-        isAuthor: false,
-        createdAt: Date.now(),
-        updatedAt: Date.now(),
-      },
-    },
-    {
-      id: 'chat_2',
-      name: 'ITStep',
-      image:
-        'https://scontent.fsof8-1.fna.fbcdn.net/v/t31.18172-8/11703541_1628435674062994_7860637593528075915_o.jpg?_nc_cat=107&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=9TzZgR3i7h8AX-4tMc1&_nc_ht=scontent.fsof8-1.fna&oh=00_AT-x9KcHqqGbl_6EQeinws4HZwhCNdzyUbHQh9A9PlQQNw&oe=629E3570',
-      lastMessage: {
-        id: 'message_id_1',
-        chatId: 'chat_2',
-        text: 'Webinar today at 5 PM',
-        author: users[2],
-        isAuthor: false,
-        createdAt: Date.now(),
-        updatedAt: Date.now(),
-      },
-    },
-  ];
 
   return res.json({ type: 'ChatsList', data: chats });
 });
